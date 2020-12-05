1fa70f363b7611478f553c7d2f6898e78bc1c7f5
Kemal Aditya Zulfikar <kadityafikar@gmail.com>
2020-12-06 04:52:09 +0700

    add promo tag

--- src/pages/products/components/CardsProduct.jsx
+++ src/pages/products/components/CardsProduct.jsx
@@ -10,17 +10,17 @@ import { X } from 'react-feather';
 class CardsProduct extends Component {
     state={
         listData : [
-            {id:'1', order:0, image: 'https://picsum.photos/id/326/200',name:' Manggo Juice', category: 'drink', price: '15000', description: 'dengan menggunakan buah asli'},
-            {id:'2', order:0, image: 'https://picsum.photos/id/324/200',name:' Apple Juice', category: 'drink', price: '15000', description: 'dengan menggunakan buah asli'},
-            {id:'3', order:0, image: 'https://picsum.photos/id/36/200',name:' Bannana Juice', category: 'drink', price: '15000', description: 'dengan menggunakan buah asli'},
-            {id:'4', order:0, image: 'https://picsum.photos/id/326/200',name:' Melon Juice', category: 'drink', price: '15000', description: 'dengan menggunakan buah asli tapi palsuuuu sdfsa sdfsa dfsaf sf sa f fs'},
-            {id:'5', order:0, image: 'https://picsum.photos/id/316/200',name:' Kiwi Juice', category: 'drink', price: '15000', description: 'dengan menggunakan buah asli'},
+            {id:'1', order:0, promo: 0, image: 'https://picsum.photos/id/326/200',name:' Manggo Juice', category: 'drink', price: '15000', description: 'dengan menggunakan buah asli'},
+            {id:'2', order:0, promo: 10, image: 'https://picsum.photos/id/324/200',name:' Apple Juice', category: 'drink', price: '15000', description: 'dengan menggunakan buah asli'},
+            {id:'3', order:0, promo: 0, image: 'https://picsum.photos/id/36/200',name:' Bannana Juice', category: 'drink', price: '15000', description: 'dengan menggunakan buah asli'},
+            {id:'4', order:0, promo: 0, image: 'https://picsum.photos/id/326/200',name:' Melon Juice', category: 'drink', price: '15000', description: 'dengan menggunakan buah asli tapi palsuuuu sdfsa sdfsa dfsaf sf sa f fs'},
+            {id:'5', order:0, promo: 20, image: 'https://picsum.photos/id/316/200',name:' Kiwi Juice', category: 'drink', price: '15000', description: 'dengan menggunakan buah asli'},
             
-            {id:'6', order:0, image: 'https://picsum.photos/id/326/200',name:' Nasi Goreng', category: 'food', price: '25000', description: 'diolah dengan bahan pilihan.'},
-            {id:'7', order:0, image: 'https://picsum.photos/id/322/200',name:' Bakso', category: 'food', price: '25000', description: 'diolah dengan bahan pilihan.'},
-            {id:'8', order:0, image: 'https://picsum.photos/id/320/200',name:' Nasi Padang', category: 'food', price: '25000', description: 'diolah dengan bahan pilihan.'},
-            {id:'9', order:0, image: 'https://picsum.photos/id/326/200',name:' Sate', category: 'food', price: '25000', description: 'diolah dengan bahan pilihan.'},
-            {id:'10',order:0, image: 'https://picsum.photos/id/326/200', name:' Bubur Ayam', category: 'food', price: '25000', description: 'diolah dengan bahan pilihan.'},
+            {id:'6', order:0, promo: 50, image: 'https://picsum.photos/id/326/200',name:' Nasi Goreng', category: 'food', price: '25000', description: 'diolah dengan bahan pilihan.'},
+            {id:'7', order:0, promo: 0, image: 'https://picsum.photos/id/322/200',name:' Bakso', category: 'food', price: '25000', description: 'diolah dengan bahan pilihan.'},
+            {id:'8', order:0, promo: 0, image: 'https://picsum.photos/id/320/200',name:' Nasi Padang', category: 'food', price: '25000', description: 'diolah dengan bahan pilihan.'},
+            {id:'9', order:0, promo: 25, image: 'https://picsum.photos/id/326/200',name:' Sate', category: 'food', price: '25000', description: 'diolah dengan bahan pilihan.'},
+            {id:'10',order:0, promo: 0, image: 'https://picsum.photos/id/326/200', name:' Bubur Ayam', category: 'food', price: '25000', description: 'diolah dengan bahan pilihan.'},
         ],
         orderPicked : [],
         modalNote : false
@@ -70,6 +70,7 @@ class CardsProduct extends Component {
         </form>
     </div>
     )
+
        
 
 
@@ -84,27 +85,35 @@ class CardsProduct extends Component {
             <Grid container spacing={2}>
                 {
                     this.state.listData.map((res, i)=>{
-                        const {name, description, price, image, id, order} = res
+                        const {name, description, price, image, id, order, promo} = res
                         return(
                         <Grid item key={i} xs={12} sm={4} >
                             <div className="card_prod" >
                                 {order !== 0 ?<div className='choosen'> <Check/> </div>: null}
-                                <div className="left">
+
+                                <div className="desc_prod">
                                     <div>
                                         <h5>{name}</h5>
                                         <p>{description}</p>
                                         {
-                                            order !== 0 ?
-                                            <h5>Rp. {price * order }</h5>
+                                            order !== 0 && promo !== 0?
+                                            <h5>Rp. {(price - (price * promo/100)) * order} promo {promo}%</h5>
                                             :
-                                            <h5>Rp. {price}</h5>
+                                            order !== 0 && promo === 0?
+                                            <h5>Rp. {price * order}</h5>
+                                            :
+                                            <h5>
+                                                Rp. 
+                                                {promo?<> <span>{price} </span> {(price - (price * promo/100))}</> : price}
+                                            </h5>
                                         }
                                     </div>
                                     <div className="note" onClick={()=> this.setState({modalNote : true})}>
                                        { order !== 0 ? <Fab><Edit /></Fab> : null} 
                                     </div>
                                 </div>
-                                <div className="right">
+
+                                <div className="image_prod">
                                     <img src={image} alt={name}/>
                                     {
                                         order !== 0?
